e71aa15d7739065e333988dd420e57a4ff0dc2e6
lapaesleme <lapaesleme@ic.uff.br>
2018-12-20 11:17:49 -0200

    Update load.sql

--- src/main/sql/load.sql
+++ src/main/sql/load.sql
@@ -10,8 +10,8 @@ COPY "NameBasics" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/name.basics.t
 ALTER TABLE "NameBasics" ADD PRIMARY KEY (nconst);
 
 
-DROP TABLE IF EXISTS "TitleAkas" CASCADE;
-CREATE TABLE "TitleAkas" (
+DROP TABLE IF EXISTS "Akas" CASCADE;
+CREATE TABLE "Akas" (
     tconst VARCHAR,
     ordering INT,
     title VARCHAR,
@@ -20,8 +20,8 @@ CREATE TABLE "TitleAkas" (
     types VARCHAR,
     attributes VARCHAR,
     "isOriginalTitle" boolean);
-COPY "TitleAkas" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.akas.tsv' DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
-ALTER TABLE "TitleAkas" ADD PRIMARY KEY (tconst,ordering);
+COPY "Akas" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.akas.tsv' DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
+ALTER TABLE "Akas" ADD PRIMARY KEY (tconst,ordering);
 
 
 DROP TABLE IF EXISTS "TitleBasics" CASCADE;
@@ -58,16 +58,16 @@ COPY "Episode" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.episode.ts
 ALTER TABLE "Episode" ADD PRIMARY KEY (tconst);
 
 
-DROP TABLE IF EXISTS "Principals" CASCADE;
-CREATE TABLE "Principals" (
+DROP TABLE IF EXISTS "Principal" CASCADE;
+CREATE TABLE "Principal" (
     tconst VARCHAR,
     ordering VARCHAR,
     nconst VARCHAR,
     category VARCHAR,
     job VARCHAR,
     characters VARCHAR);
-COPY "Principals" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.principals.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
-ALTER TABLE "Principals" ADD PRIMARY KEY (tconst,nconst,ordering);
+COPY "Principal" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.principals.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
+ALTER TABLE "Principal" ADD PRIMARY KEY (tconst,nconst,ordering);
 
 
 DROP TABLE IF EXISTS "Rating" CASCADE;
@@ -85,25 +85,25 @@ CREATE TABLE "Name" AS
 SELECT nconst FROM "NameBasics";
 ALTER TABLE "Name" ADD PRIMARY KEY (nconst);
 
-INSERT INTO "Name"(nconst)  (SELECT DISTINCT t1.nconst FROM "Principals" t1 LEFT JOIN "Name" t2 ON t1.nconst = t2.nconst WHERE t2.nconst IS NULL);
+INSERT INTO "Name"(nconst)  (SELECT DISTINCT t1.nconst FROM "Principal" t1 LEFT JOIN "Name" t2 ON t1.nconst = t2.nconst WHERE t2.nconst IS NULL);
 
-ALTER TABLE "Principals" ADD FOREIGN KEY (nconst) REFERENCES "Name";
+ALTER TABLE "Principal" ADD FOREIGN KEY (nconst) REFERENCES "Name";
 -------------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS "Title" CASCADE;
 CREATE TABLE "Title" AS
 SELECT tconst FROM "TitleBasics"
-UNION SELECT tconst FROM "TitleAkas";
+UNION SELECT tconst FROM "Akas";
 ALTER TABLE "Title" ADD PRIMARY KEY (tconst);
 
 INSERT INTO "Title"(tconst)  (SELECT DISTINCT t1."parentTconst" FROM "Episode" t1 LEFT JOIN "Title" t2 ON t1."parentTconst" = t2.tconst WHERE t2.tconst IS NULL);
-INSERT INTO "Title"(tconst)  (SELECT DISTINCT t1.tconst FROM "Principals" t1 LEFT JOIN "Title" t2 ON t1.tconst = t2.tconst WHERE t2.tconst IS NULL);
+INSERT INTO "Title"(tconst)  (SELECT DISTINCT t1.tconst FROM "Principal" t1 LEFT JOIN "Title" t2 ON t1.tconst = t2.tconst WHERE t2.tconst IS NULL);
 
-ALTER TABLE "TitleAkas" ADD FOREIGN KEY (tconst) REFERENCES "Title";
+ALTER TABLE "Akas" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "TitleBasics" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "TitleCrew" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "Episode" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "Episode" ADD FOREIGN KEY ("parentTconst") REFERENCES "Title";
-ALTER TABLE "Principals" ADD FOREIGN KEY (tconst) REFERENCES "Title";
+ALTER TABLE "Principal" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "Rating" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 -------------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS "Profession" CASCADE;
@@ -119,14 +119,14 @@ WITH
     t1 AS (SELECT DISTINCT nconst, trim(regexp_split_to_table("primaryProfession", ',+')) AS pconst FROM "NameBasics")
 SELECT DISTINCT nconst,pconst FROM t1 WHERE pconst is not null and pconst != '';
 
-ALTER TABLE "Principals" ADD COLUMN IF NOT EXISTS pconst VARCHAR;
-UPDATE  "Principals" SET pconst = 'id-' || md5(trim(category))::uuid;
-INSERT INTO "Profession"(pconst,label)  (SELECT DISTINCT 'id-' || md5(trim(category))::uuid, trim(category) FROM "Principals" t1 LEFT JOIN "Profession" t2 ON 'id-' || md5(trim(category))::uuid = t2.pconst WHERE t2.pconst IS NULL);
+ALTER TABLE "Principal" ADD COLUMN IF NOT EXISTS pconst VARCHAR;
+UPDATE  "Principal" SET pconst = 'id-' || md5(trim(category))::uuid;
+INSERT INTO "Profession"(pconst,label)  (SELECT DISTINCT 'id-' || md5(trim(category))::uuid, trim(category) FROM "Principal" t1 LEFT JOIN "Profession" t2 ON 'id-' || md5(trim(category))::uuid = t2.pconst WHERE t2.pconst IS NULL);
 
 ALTER TABLE "primaryProfession" ADD PRIMARY KEY (nconst,pconst);
 ALTER TABLE "primaryProfession" ADD FOREIGN KEY (nconst) REFERENCES "NameBasics";
 ALTER TABLE "primaryProfession" ADD FOREIGN KEY (pconst) REFERENCES "Profession";
-ALTER TABLE "Principals" ADD FOREIGN KEY (pconst) REFERENCES "Profession";
+ALTER TABLE "Principal" ADD FOREIGN KEY (pconst) REFERENCES "Profession";
 -------------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS "knownForTitle" CASCADE;
 CREATE TABLE "knownForTitle" AS
@@ -173,6 +173,6 @@ ALTER TABLE "Crew" ADD FOREIGN KEY (pconst) REFERENCES "Profession";
 ALTER TABLE "NameBasics" DROP COLUMN IF EXISTS "primaryProfession";
 ALTER TABLE "NameBasics" DROP COLUMN IF EXISTS "knownForTitles";
 ALTER TABLE "TitleBasics" DROP COLUMN IF EXISTS genres;
-ALTER TABLE "Principals" DROP COLUMN IF EXISTS category;
+ALTER TABLE "Principal" DROP COLUMN IF EXISTS category;
 DROP TABLE IF EXISTS "TitleCrew" CASCADE;
 -------------------------------------------------------------------------------------------------------------------------
