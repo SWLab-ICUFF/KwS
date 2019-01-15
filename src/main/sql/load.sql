b8ec50dbc4b44b5da51f0eca739997ba30e3ff30
Luiz Leme <lapaesleme@ic.uff.br>
2019-01-15 18:37:55 -0200

    ok

--- src/main/sql/load.sql
+++ src/main/sql/load.sql
@@ -9,6 +9,12 @@ CREATE TABLE "NameBasics" (
 COPY "NameBasics" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/name.basics.tsv' DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "NameBasics" ADD PRIMARY KEY (nconst);
 
+COMMENT ON TABLE "NameBasics" IS 'Contains the following information for names';
+COMMENT ON COLUMN "NameBasics".nconst IS 'alphanumeric unique identifier of the name/person';
+COMMENT ON COLUMN "NameBasics"."primaryName" IS 'name by which the person is most often credited';
+COMMENT ON COLUMN "NameBasics"."birthYear" IS 'in YYYY format';
+COMMENT ON COLUMN "NameBasics"."deathYear" IS 'in YYYY format if applicable, else \N';
+
 
 DROP TABLE IF EXISTS "Akas" CASCADE;
 CREATE TABLE "Akas" (
@@ -22,6 +28,16 @@ CREATE TABLE "Akas" (
     "isOriginalTitle" boolean);
 COPY "Akas" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.akas.tsv' DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "Akas" ADD PRIMARY KEY (tconst,ordering);
+
+COMMENT ON TABLE "Akas" IS 'Contains the following information for titles';
+COMMENT ON COLUMN "Akas".tconst IS 'a tconst, an alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "Akas".ordering IS 'a number to uniquely identify rows for a given titleId';
+COMMENT ON COLUMN "Akas".title IS 'the localized title';
+COMMENT ON COLUMN "Akas".region IS 'the region for this version of the title';
+COMMENT ON COLUMN "Akas"."language" IS 'the language of the title'; 
+COMMENT ON COLUMN "Akas".types IS 'Enumerated set of attributes for this alternative title. One or more of the following: "alternative", "dvd", "festival", "tv", "video", "working", "original", "imdbDisplay". New values may be added in the future without warning'; 
+COMMENT ON COLUMN "Akas".attributes IS 'Additional terms to describe this alternative title, not enumerated'; 
+COMMENT ON COLUMN "Akas"."isOriginalTitle" IS '0: not original title; 1: original title'; 
 
 
 DROP TABLE IF EXISTS "TitleBasics" CASCADE;
@@ -37,6 +53,16 @@ CREATE TABLE "TitleBasics" (
     genres VARCHAR); -- split
 COPY "TitleBasics" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.basics.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "TitleBasics" ADD PRIMARY KEY (tconst);
+
+COMMENT ON TABLE "TitleBasics" IS 'Contains the following information for titles';
+COMMENT ON COLUMN "TitleBasics".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "TitleBasics"."titleType" IS 'the type/format of the title (e.g. movie, short, tvseries, tvepisode, video, etc)';
+COMMENT ON COLUMN "TitleBasics"."primaryTitle" IS 'the more popular title / the title used by the filmmakers on promotional materials at the point of release';
+COMMENT ON COLUMN "TitleBasics"."originalTitle" IS 'original title, in the original language';
+COMMENT ON COLUMN "TitleBasics"."isAdult" IS '0: non-adult title; 1: adult title';
+COMMENT ON COLUMN "TitleBasics"."startYear" IS 'represents the release year of a title. In the case of TV Series, it is the series start year';
+COMMENT ON COLUMN "TitleBasics"."endYear" IS 'TV Series end year. ‘\N’ for all other title types';
+COMMENT ON COLUMN "TitleBasics"."runtimeMinutes" IS 'primary runtime of the title, in minutes';
 
 
 DROP TABLE IF EXISTS "TitleCrew" CASCADE;
@@ -57,6 +83,12 @@ CREATE TABLE "Episode" (
 COPY "Episode" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.episode.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "Episode" ADD PRIMARY KEY (tconst);
 
+COMMENT ON TABLE "Episode" IS 'Contains the tv episode information.';
+COMMENT ON COLUMN "Episode".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "Episode"."parentTconst" IS 'alphanumeric identifier of the parent TV Series';
+COMMENT ON COLUMN "Episode"."seasonNumber" IS 'season number the episode belongs to';
+COMMENT ON COLUMN "Episode"."episodeNumber" IS 'episode number of the tconst in the TV series';
+
 
 DROP TABLE IF EXISTS "Principal" CASCADE;
 CREATE TABLE "Principal" (
@@ -69,6 +101,13 @@ CREATE TABLE "Principal" (
 COPY "Principal" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.principals.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "Principal" ADD PRIMARY KEY (tconst,nconst,ordering);
 
+COMMENT ON TABLE "Principal" IS 'Contains the principal cast/crew for titles.';
+COMMENT ON COLUMN "Principal".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "Principal".ordering IS 'a number to uniquely identify rows for a given titleId';
+COMMENT ON COLUMN "Principal".nconst IS 'alphanumeric unique identifier of the name/person';
+COMMENT ON COLUMN "Principal".job IS 'the specific job title if applicable, else \N';
+COMMENT ON COLUMN "Principal".characters IS 'the name of the character played if applicable, else \N';
+
 
 DROP TABLE IF EXISTS "Rating" CASCADE;
 CREATE TABLE "Rating" (
@@ -77,6 +116,12 @@ CREATE TABLE "Rating" (
     "numVotes" INT);
 COPY "Rating" FROM '/Users/lapaesleme/OneDrive/Data/dumps/IMDb/title.ratings.tsv' WITH DELIMITER E'\t' NULL '\N' CSV HEADER QUOTE E'\b';
 ALTER TABLE "Rating" ADD PRIMARY KEY (tconst);
+
+COMMENT ON TABLE "Rating" IS 'Contains the IMDb rating and votes information for titles.';
+COMMENT ON COLUMN "Rating".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "Rating"."averageRating" IS 'weighted average of all the individual user ratings';
+COMMENT ON COLUMN "Rating"."numVotes" IS 'number of votes the title has received';
+-------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------
 -------------------------------------------------------------------------------------------------------------------------
@@ -112,16 +157,23 @@ WITH
     t1 AS (SELECT DISTINCT trim(regexp_split_to_table("primaryProfession", ',+')) AS pconst FROM "NameBasics")
 SELECT pconst, pconst AS label FROM t1 WHERE pconst is not null and pconst != '';
 ALTER TABLE "Profession" ADD PRIMARY KEY (pconst);
+COMMENT ON TABLE "Profession" IS 'Contains professions.';
+COMMENT ON COLUMN "Profession".pconst IS 'alphanumeric unique identifier of the profession';
 
 DROP TABLE IF EXISTS "primaryProfession" CASCADE;
 CREATE TABLE "primaryProfession" AS
 WITH
     t1 AS (SELECT DISTINCT nconst, trim(regexp_split_to_table("primaryProfession", ',+')) AS pconst FROM "NameBasics")
 SELECT DISTINCT nconst,pconst FROM t1 WHERE pconst is not null and pconst != '';
+COMMENT ON TABLE "primaryProfession" IS 'professions of a person';
+COMMENT ON COLUMN "NameBasics".nconst IS 'alphanumeric unique identifier of the name/person';
+COMMENT ON COLUMN "primaryProfession".pconst IS 'alphanumeric unique identifier of the profession';
 
 ALTER TABLE "Principal" ADD COLUMN IF NOT EXISTS pconst VARCHAR;
+COMMENT ON COLUMN "Principal"."pconst" IS 'alphanumeric unique identifier of the category of job that person was in';
 UPDATE  "Principal" SET pconst = 'id-' || md5(trim(category))::uuid;
 INSERT INTO "Profession"(pconst,label)  (SELECT DISTINCT 'id-' || md5(trim(category))::uuid, trim(category) FROM "Principal" t1 LEFT JOIN "Profession" t2 ON 'id-' || md5(trim(category))::uuid = t2.pconst WHERE t2.pconst IS NULL);
+
 
 ALTER TABLE "primaryProfession" ADD PRIMARY KEY (nconst,pconst);
 ALTER TABLE "primaryProfession" ADD FOREIGN KEY (nconst) REFERENCES "NameBasics";
@@ -133,6 +185,9 @@ CREATE TABLE "knownForTitle" AS
 with
     t1 AS (SELECT DISTINCT nconst,trim(regexp_split_to_table("knownForTitles", ',+')) AS tconst FROM "NameBasics")
 SELECT nconst,tconst FROM t1 WHERE tconst is not null and tconst != '';
+COMMENT ON TABLE "knownForTitle" IS 'itles the person is known for.';
+COMMENT ON COLUMN "knownForTitle".nconst IS 'alphanumeric unique identifier of the name/person';
+COMMENT ON COLUMN "knownForTitle".tconst IS 'alphanumeric unique identifier of the title';
 
 INSERT INTO "Title"(tconst)  (SELECT DISTINCT t1.tconst FROM "knownForTitle" t1 LEFT JOIN "Title" t2 ON t1.tconst = t2.tconst WHERE t2.tconst IS NULL);
 
@@ -146,11 +201,17 @@ WITH
     t1 AS (SELECT DISTINCT trim(regexp_split_to_table(genres, ',+')) AS gconst FROM "TitleBasics")
 SELECT gconst, gconst AS label FROM t1 WHERE gconst is not null and gconst != '';
 ALTER TABLE "Genre" ADD PRIMARY KEY (gconst);
+COMMENT ON TABLE "Genre" IS 'genres of titles';
+COMMENT ON COLUMN "Genre".gconst IS 'alphanumeric unique identifier of the genre';
+COMMENT ON COLUMN "Genre".label IS 'label of the genre';
 
 DROP TABLE IF EXISTS "hasGenre" CASCADE;
 CREATE TABLE "hasGenre" AS
 SELECT DISTINCT tconst,trim(regexp_split_to_table(genres, ',+')) AS gconst FROM "TitleBasics";
 ALTER TABLE "hasGenre" ADD PRIMARY KEY (tconst,gconst);
+COMMENT ON TABLE "hasGenre" IS 'genres associated with titles';
+COMMENT ON COLUMN "knownForTitle".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "Genre".gconst IS 'alphanumeric unique identifier of the genre';
 
 ALTER TABLE "hasGenre" ADD FOREIGN KEY (tconst) REFERENCES "Title";
 ALTER TABLE "hasGenre" ADD FOREIGN KEY (gconst) REFERENCES "Genre";
@@ -162,6 +223,10 @@ WITH
            UNION SELECT DISTINCT tconst,trim(regexp_split_to_table(writers, ',+')) AS nconst, 'writer' AS pconst FROM "TitleCrew")
 SELECT tconst,nconst,pconst FROM t1 WHERE nconst is not null and nconst != '';
 ALTER TABLE "Crew" ADD PRIMARY KEY (tconst,nconst,pconst);
+COMMENT ON TABLE "Crew" IS 'Contains the director and writer information for all the titles in IMDb.';
+COMMENT ON COLUMN "Crew".tconst IS 'alphanumeric unique identifier of the title';
+COMMENT ON COLUMN "knownForTitle".nconst IS 'alphanumeric unique identifier of the name/person';
+COMMENT ON COLUMN "Crew".pconst IS 'alphanumeric unique identifier of the profession';
 
 INSERT INTO "Name"(nconst)  (SELECT DISTINCT t1.nconst FROM "Crew" t1 LEFT JOIN "Name" t2 ON t1.nconst = t2.nconst WHERE t2.nconst IS NULL);
 
@@ -176,3 +241,4 @@ ALTER TABLE "TitleBasics" DROP COLUMN IF EXISTS genres;
 ALTER TABLE "Principal" DROP COLUMN IF EXISTS category;
 DROP TABLE IF EXISTS "TitleCrew" CASCADE;
 -------------------------------------------------------------------------------------------------------------------------
+UPDATE "Akas" SET types = trim(regexp_replace(replace(replace(replace(replace(replace(replace(replace(replace(types,'alternative','alternative '),'dvd','dvd '),'festival','festival '),'tv','tv '),'video','video '),'working','working '),'original','original '),'imdbDisplay','imdbDisplay '),'\s+',' ', 'g'));
