1b15c36ff7a2ce682d63ca9d92b2a22846a2cfcc
lapaesleme <lapaesleme@ic.uff.br>
2019-04-06 11:50:08 -0300

    ok

--- src/main/java/uff/ic/lleme/kws/imdb/RunKWSQuery.java
+++ src/main/java/uff/ic/lleme/kws/imdb/RunKWSQuery.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.time.Duration;
+import java.util.Calendar;
 import javax.naming.InvalidNameException;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.RDFDataMgr;
@@ -16,9 +18,10 @@ public class RunKWSQuery {
 
     public static void main(String[] args) throws FileNotFoundException, IOException, InvalidNameException {
         FusekiServer fuseki = new FusekiServer("lappl-mm", 3030);
-        String kwsString = "Brazil rio janeiro";
+        String kwsString = "mauritius india";
         String queryString = "";
 
+        Calendar t1 = Calendar.getInstance();
         if (true) {
             queryString = readQuery("./src/main/sparql/kws_search_1.rq");
             queryString = queryString.format(queryString, kwsString);
@@ -35,6 +38,20 @@ public class RunKWSQuery {
             queryString = readQuery("./src/main/sparql/kws_rank.rq");
             queryString = queryString.format(queryString, kwsString);
             fuseki.execUpdate(queryString, "Work.temp");
+        }
+        Calendar t2 = Calendar.getInstance();
+        System.out.println(String.format("Elapsed time: %1$d seconds", Duration.between(t1.toInstant(), t2.toInstant()).toSeconds()));
+
+        {
+            Model model = fuseki.getModel("Work.temp", "urn:graph:kws:seeds");
+            model.setNsPrefix("urn", "urn:uuid:");
+            model.setNsPrefix("kws", "urn:vocab:kws:");
+            model.setNsPrefix("kwsg", "urn:graph:kws:");
+            model.setNsPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            model.setNsPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+            model.setNsPrefix("mond", "http://www.semwebtech.org/mondial/10/");
+            model.setNsPrefix("meta", "http://www.semwebtech.org/mondial/10/meta#");
+            writeModel(model, "./src/main/resources/dat/seeds.ttl");
         }
 
         {
@@ -71,6 +88,18 @@ public class RunKWSQuery {
             model.setNsPrefix("mond", "http://www.semwebtech.org/mondial/10/");
             model.setNsPrefix("meta", "http://www.semwebtech.org/mondial/10/meta#");
             writeModel(model, "./src/main/resources/dat/temp3.ttl");
+        }
+
+        {
+            Model model = fuseki.getModel("Work.temp", "urn:graph:kws:temp4");
+            model.setNsPrefix("urn", "urn:uuid:");
+            model.setNsPrefix("kws", "urn:vocab:kws:");
+            model.setNsPrefix("kwsg", "urn:graph:kws:");
+            model.setNsPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            model.setNsPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+            model.setNsPrefix("mond", "http://www.semwebtech.org/mondial/10/");
+            model.setNsPrefix("meta", "http://www.semwebtech.org/mondial/10/meta#");
+            writeModel(model, "./src/main/resources/dat/temp4.ttl");
         }
 
         {
