8d7e31f059cc6c58a5e60796fbf3e18ce75830fc
lapaesleme <lapaesleme@ic.uff.br>
2019-05-15 16:24:44 -0300

    ok

--- src/main/java/uff/ic/swlab/kws/BuildBenchmark.java
+++ src/main/java/uff/ic/swlab/kws/BuildBenchmark.java
@@ -31,7 +31,7 @@ public class BuildBenchmark {
         String service3 = "http://localhost:3030/KwS.temp/sparql";
         String kwsVersion;
         String benchmark;
-        if (false) {
+        if (true) {
             kwsVersion = "v2";
             benchmark = "CIKM2019_1";
         } else {
