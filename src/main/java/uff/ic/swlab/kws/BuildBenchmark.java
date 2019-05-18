64f210983626cbcbdf1b53b8a9374e379eaf99ec
lapaesleme <lapaesleme@ic.uff.br>
2019-05-18 12:01:10 -0300

    ok

--- src/main/java/uff/ic/swlab/kws/BuildBenchmark.java
+++ src/main/java/uff/ic/swlab/kws/BuildBenchmark.java
@@ -118,7 +118,7 @@ public class BuildBenchmark {
             fuseki.execUpdate(queryString, "KwS.stats");
         }
 
-        if (false) {
+        if (true) {
             queryString = readQuery(String.format("./src/main/sparql/KwS/%1$s/kws_60_finish.rq", kwsVersion));
             queryString = queryString.format(queryString, service1, service2, benchmarkNS);
             fuseki.execUpdate(queryString, "KwS.temp");
