9b1e1ee31446f6219a83bb90c21717a7910bad0f
Luiz Leme <lapaesleme@ic.uff.br>
2020-12-16 12:52:28 -0300

    no message

--- src/main/java/uff/ic/swlab/util/StainerTree/Main.java
+++
@@ -1,9 +0,0 @@
-package uff.ic.swlab.util.StainerTree;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String s = "fff";
-        System.out.println(s.equals(1));
-    }
-}
